9929e881c81080cefe824a1888bac43a74f43883
krali@kth.se <krali@kth.se>
2018-02-15 15:35:36 +0100

    Adding a bash script, which generates a project template folder for our project course.

---
+++ template_project.sh
@@ -0,0 +1,67 @@
+
+#Template for my project in Molecular Life Science 
+#(the README.md file is already created along with the directory)
+
+#Create a project folder
+
+mkdir My_Project
+
+cd My_Project
+
+#Create a bash file that contains all the codes and runs everything
+
+touch runall.sh
+
+touch README.md
+
+echo "Template for the the project in Molecular Life Science course. Prediction of the structure of proteins. Feature predictor development" > README.md
+
+# Create the directories I will need
+
+mkdir Datasets 
+
+mkdir Results
+
+mkdir Figures #for all the figures/plots/graphs I would may get
+
+mkdir Bin #any small scripts that include necessary paths
+
+mkdir Source_codes #core scripts for the feature predictor development
+
+cd Source_codes
+
+touch predictor.py #empty python file where I will store my code for the predictor
+
+cd 
+
+cd My_Project
+
+mkdir Docs #papers, instructions etc. are stored
+
+cd Docs
+
+mkdir Papers_list
+
+cd
+
+cd My_Project
+
+mkdir Diary #daily diary that tracks the project
+
+cd Diary
+
+
+#Make subdirectories for the diaries for each week(each week will contain a report for everyday in a txt file folder)
+
+mkdir Week2
+mkdir Week3
+mkdir Week4
+mkdir Week5
+
+
+
+ 
+
+
+
+
