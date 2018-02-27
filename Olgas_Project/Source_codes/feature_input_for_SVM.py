d706fad1ed360e192bb715bb906c1c66593e0947
Olga Krali <35502239+olgakrali@users.noreply.github.com>
2018-02-27 20:17:26 +0100

    new code

--- Olgas_Project/Source_codes/feature_input_for_SVM.py
+++ Olgas_Project/Source_codes/feature_input_for_SVM.py
@@ -1,11 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-import matplotlib.pyplot as plt
-
 
 path = "C:/Users/olga_/Documents/Master's in molecular techniques in life science/Project in Molecular Life Science/"
 
