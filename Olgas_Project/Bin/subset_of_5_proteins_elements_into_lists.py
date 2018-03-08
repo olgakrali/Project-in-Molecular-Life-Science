65e68be21bfb6310c105f7c926c5e62738b88deb
Olga Krali <35502239+olgakrali@users.noreply.github.com>
2018-03-08 18:48:21 +0100

    Update subset_of_5_proteins_elements_into_lists.py

--- Olgas_Project/Bin/subset_of_5_proteins_elements_into_lists.py
+++ Olgas_Project/Bin/subset_of_5_proteins_elements_into_lists.py
@@ -4,23 +4,27 @@
 
 path = 'Datasets/'
 
-protein_ID = []
-sequence = []
-topology = []
+def my_par(filename: str):
 
-with open (path + "subset_of_5_proteins.txt") as f:
-    count = 2                                          # the counter will give me the 2nd line
-    for number, line in enumerate(f, start = 1):
-        if line.startswith('>'):
-            protein_id = line[1:].rstrip()
-            protein_ID.append(protein_id)
-        elif number == count:
-            seq = line.rstrip()
-            sequence.append(seq)
-            count =  count + 3        ### count starts from line 2 and will add 3 (that line of code will give as an output every protein sequence only)
-        else:
-            feature = line.rstrip()
-            topology.append(feature)
-print(protein_ID)
-print(sequence)
-print(topology)
+    protein_ID = []
+    sequences = []
+    topology = []
+    with open(path + filename) as f:
+        count = 2  # gives me the second line directly
+        for number, line in enumerate(f, start=1):
+            if line.startswith('>'):
+                protein_id = line[1:].rstrip()
+                protein_ID.append(protein_id)
+            elif number == count:
+                seq = line.rstrip()
+                sequences.append(seq)
+                count = count + 3  ### if you add 3 starting from line 2 you will always get the sequence of a protein as an output
+            else:
+                feature = line.rstrip()
+                topology.append(feature)
+    return protein_ID, sequences, topology
+
+
+if __name__ == "__name__":
+    protein_ID, sequences, topology = my_par("subset_of_5_proteins.txt")
+    print(topology)
