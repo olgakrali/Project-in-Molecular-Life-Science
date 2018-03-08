105db82b9af817be5db5542d1a1baa53feca3e36
Olga Krali <35502239+olgakrali@users.noreply.github.com>
2018-03-08 18:54:47 +0100

    Update one_hot_encoder_newVersion.py

--- Olgas_Project/Bin/one_hot_encoder_newVersion.py
+++ Olgas_Project/Bin/one_hot_encoder_newVersion.py
@@ -1,29 +1,32 @@
-#########One hot encoder updated version
+######### One hot encoder updated version
 
-aminoacid = list('ACDEFGHIKLMNPQRSTVWY')  ### a list for the 20 amino acids
+def my_one_hot_enc():
+    aminoacid = list('ACDEFGHIKLMNPQRSTVWY')  ### a list for the 20 amino acids
 
-#Make a list that contains 20 zeros
-zeros_list = []
+    # Make a list that contains 20 zeros
+    zeros_list = []
 
-for line in aminoacid:
-    n = 20
-    zeros = [0]*n
-    zeros_list.append(zeros)
-#print(zeros_list)
+    for i in range(len(aminoacid)):
+        n = 20
+        zeros = [0] * n
+        zeros_list.append(zeros)
+    # print(zeros_list)
 
-### Make one hot encoders for each amino acid
-zero_one = []
-i = 0
-for zero in zeros_list:
-    zero[i] = 1
-    i = i + 1
-    zero_one.append(zero)
-print(zero_one)
+    ### Make one hot encoders for each amino acid
+    zero_one = []
+    j = 0
+    for zero in zeros_list:
+        zero[j] = 1
+        j = j + 1
+        zero_one.append(zero)
+    # print(zero_one)
 
 
-# Make a dictionary to add its keys and values
-dictionary = {}
-for line, aa in zip(zero_one, aminoacid):     ### it reads simultaneously every row and every amino acid
-    dictionary[aa] = line
+    # Make a dictionary to add its keys and values
+    dictionary = {}
+    for line, aa in zip(zero_one, aminoacid):  ### it reads simultaneously every row and every amino acid
+        dictionary[aa] = line
+    return dictionary
 
-print(dictionary)
+if __name__ == "__main__":
+    print(my_one_hot_enc())
