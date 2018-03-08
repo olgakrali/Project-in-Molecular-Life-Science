12ee21269f061159478328ce14ce88d8eda7e522
Olga Krali <35502239+olgakrali@users.noreply.github.com>
2018-03-08 19:32:31 +0100

    Update topology_labels.py

--- Olgas_Project/Bin/topology_labels.py
+++ Olgas_Project/Bin/topology_labels.py
@@ -4,17 +4,18 @@
 
 topologies = ['GGGGGGGGGGGGGGGGGGGGGGGGGSSSSSSSS','GGGGGGGGGGGGGGGGGGGGGGGGGGGG','MMMMMMMMMMMMMMMMMMMMMMMMMMM','SSSSSSSSSSMMMMMMMMMMMSSSSSSSSSSS']
 
-# Assign a number to each of the three structures
+def my_labels():
+    struct_labels = {'G': 1, 'S': 2, 'M': 3}
+    seconstr = []  # this list will contain a list of integers for every protein
+    for structure in topologies:
+        list_A = []
+        for letter in structure:
+            for key, value in struct_labels.items():
+                if letter == key:
+                    list_A.append(value)
+        seconstr.extend(list_A)  #### add the topologies from every sequence together in one list
+    #print(len(seconstr))
+    return seconstr
 
-struct_labels = {'G': 1, 'S': 2, 'M': 3}
-
-structure = []         #this list will contain a list of integers for every protein ( 5 lists in this case)
-for topology in topologies:
-    list_A = []
-    for letter in topology:
-        for key, value in struct_labels.items():
-            if letter == key:
-                list_A.append(value)
-    structure.append(list_A)
-    print(list_A)   ##prints out each topology sequence transformed into integers
-print(structure)    ## list of all numeric topologies in a larger list
+if __name__ == "__main__":
+    print(my_labels())
