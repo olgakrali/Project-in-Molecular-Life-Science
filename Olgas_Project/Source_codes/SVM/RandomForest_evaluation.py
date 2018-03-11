cbe558c97e4c787c4e3e1dd3d4b0b2f6d11ca742
Olga Krali <krali@kth.se>
2018-03-11 21:24:51 +0100

    new addings

---
+++ Olgas_Project/Source_codes/SVM/RandomForest_evaluation.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+
+import parsers_windows as pw
+
+path = "Datasets/"
+
+
+classifier = []
+for windows in range(5,37,4):
+    protein_ID, sequence, topology, svm2, seconstr, struct_labels = pw.my_par('eval_subset.txt', windows)
+    X = np.array(svm2)
+    #print(X.shape)
+    y = np.array(seconstr)
+    #print(y.shape)
+    for n_estimators in range(100,400,50):
+        for min_samples_split in range(2,10):
+            forest = RandomForestClassifier(n_estimators = n_estimators, min_samples_split = min_samples_split)
+            forest.fit(X,y)
+            scores = cross_val_score(forest,X, y, cv=5, verbose=True)
+            scores2  = np.average(scores)
+            print(scores2)
+            classifier.append((windows, n_estimators, min_samples_split, forest, scores2))
+
+print(classifier)
