a72a51ea45ad30480ca5f359bc1fd6da21c3bf02
Olga Krali <krali@kth.se>
2018-03-13 22:36:12 +0100

    new files

---
+++ Olgas_Project/Source_codes/SVM/save_svm_PSSM.py
@@ -0,0 +1,19 @@
+import os
+import PSSM_parsing as ps
+from sklearn.svm import SVC
+import pickle
+path = "Datasets/"
+path2 = "PSSM/train/pssm/"
+
+fnames = os.listdir(path2)
+
+my_names, new_seq, new_top, struct_labels, X_train, y_train =  ps.my_par(path + "60_prot.txt",fnames, 19, path2)
+
+print(X_train.shape)
+print(y_train.shape)
+
+clfr = SVC(kernel = 'linear', C = 1, gamma = 0.01, cache_size = 3000)
+
+clfr.fit(X_train,y_train)
+
+pickle.dump(clfr, open(path + "my_pssm_test.plk", "wb"))
